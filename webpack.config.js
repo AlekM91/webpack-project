a5dc1d50b0b57a1e4163b35f8184cfa82de5bbbc
Aleksandar Mitrevski <aleksandarmitrevski91@gmail.com>
2021-11-16 12:05:03 +0100

    initial config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src/scripts/main.js')
+    },
+    output: {
+        filename: "bundled.js",
+        path: path.resolve(__dirname, 'src'),
+    },
+    mode: "development"
+}
